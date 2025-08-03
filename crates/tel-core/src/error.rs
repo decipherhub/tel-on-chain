5cb6443faefea1aa0b1f55abe8e1a95d61f44052
ghXst <113441721+0xGh-st@users.noreply.github.com>
2025-08-03 22:31:08 +0900

    Feat/get v3 liquidity distribution (#23)
    * impl ing get_v3_liquidity_distribution
    
    * working: get_v3_liquidity_distribution
    
    * impl get_v3_liquidity_distribution_with_sdk
    
    * checkpoint
    
    * rebase and checkpoint
    
    * checkpoint of get_v3_liquidity_distribution
    
    * modify get_v3_liquidity_distribution
    
    * modify .env.example

--- crates/tel-core/src/error.rs
+++ crates/tel-core/src/error.rs
@@ -37,10 +37,50 @@ pub enum Error {
 
     #[error("Not implemented")]
     NotImplemented,
+
+    #[error("Uniswap V3 SDK error: {0}")]
+    UniswapV3Error(String),
+
+    #[error("Uniswap SDK Core error: {0}")]
+    UniswapCoreError(String),
+
+    #[error("Conversion error: {0}")]
+    ConversionError(String),
 }
 
 impl From<rusqlite::Error> for Error {
     fn from(err: rusqlite::Error) -> Self {
         Error::DatabaseError(err.to_string())
+    }
+}
+
+impl From<uniswap_v3_sdk::error::Error> for Error {
+    fn from(err: uniswap_v3_sdk::error::Error) -> Self {
+        Error::UniswapV3Error(err.to_string())
+    }
+}
+
+impl From<uniswap_sdk_core::error::Error> for Error {
+    fn from(err: uniswap_sdk_core::error::Error) -> Self {
+        Error::UniswapCoreError(err.to_string())
+    }
+}
+
+impl From<std::num::TryFromIntError> for Error {
+    fn from(err: std::num::TryFromIntError) -> Self {
+        Error::ConversionError(err.to_string())
+    }
+}
+
+// Generic implementation for any conversion error
+impl From<Box<dyn std::error::Error + Send + Sync>> for Error {
+    fn from(err: Box<dyn std::error::Error + Send + Sync>) -> Self {
+        Error::ConversionError(err.to_string())
+    }
+}
+
+impl From<std::convert::Infallible> for Error {
+    fn from(_: std::convert::Infallible) -> Self {
+        Error::ConversionError("Infallible error".to_string())
     }
 }
